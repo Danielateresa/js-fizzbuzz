30423613304d0742780e4f656cb1301d8885e3f3
Danielateresa <dany.t_87@hotmail.it>
2022-10-10 13:38:54 +0200

    Initial commit FizzBuzz

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*
+TRACCIA
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+Consigli del giorno:
+Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+La condizione non funziona? controlliamo il loro ordine.*/
+
+alert('ciao')
+
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+//comincio realizzando il loop col ciclo for
+
+
+//Se il numero di quelli stampati è divisibile per 3 stampa “Fizz” al posto del numero
+//il numero divisibile per 3 deve dare come resto 0
+
+//Se il numero di quelli stampati è divisibile per 5 stampa “Buzz” al posto del numero
+//il numero divisibile per 5 deve dare come resto 0
+
+//Se il numero di quelli stampati è sia multiplo di 3 che di 5 stampi “FizzBuzz”.
+
+//TIPS
+//per dividere un numero potrei usare % con la condizione if
